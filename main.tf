705d049e9a1cf1542b41299e69529d056d8201a4
ttnsheep <ttnsheep@gmail.com>
2022-07-09 13:30:26 +0900

    ３章

--- main.tf
+++ main.tf
@@ -1,6 +1,10 @@
+variable "example_instance_type" {
+    default = "t3.micro"
+}
+
 resource "aws_instance" example {
     ami = "ami-0c3fd0f5d33134a76"
-    instance_type = "t3.micro"
+    instance_type = var.example_instance_type
 
     tags = {
       "Name" = "example"
@@ -12,4 +16,18 @@ resource "aws_instance" example {
         yum install -y httpd
         systemctl start httpd.service
     EOF
+ }
+
+data "aws_ami" "recent_amazon_linux_2" {
+    most_recent = true
+    owners = ["amazon"]
+    filter {
+        name    = "name"
+        values  = ["amzn2-ami-hvm-2.0.20190618-x86_64-gp2"]
+    }
+    
+}
+
+ output "example_instance_id" {
+    value = aws_instance.example.id
  }
